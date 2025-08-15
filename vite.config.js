eb913882c3176181f538618fce6fc16dbc4f30d8
khatiwadaprajwal <prajwalkhatiwada28@gmail.com>
2025-08-15 21:14:01 +0545

    v

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+   base: '/portfolio-site/',
   plugins: [
+   
     react(),
     tailwindcss(),
   ],
